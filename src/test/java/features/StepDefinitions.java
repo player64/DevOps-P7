0a6c324c446cbb18550f64a8a781623c027774be
mariusz januszkiewicz <player.c64@gmail.com>
2021-09-11 15:46:44 +0100

    Cleaned the step definition

--- src/test/java/features/StepDefinitions.java
+++ src/test/java/features/StepDefinitions.java
@@ -12,50 +12,36 @@ import revolut.Person;
 public class StepDefinitions {
 
     private double topUpAmount;
-    //private String topUpMethod;
     PaymentService topUpMethod;
     Person danny;
 
-    @Before//Before hooks run before the first step in each scenario
+    @Before
     public void setUp(){
-        //We can use this to setup test data for each scenario
         danny = new Person("Danny");
     }
     @Given("Danny has {double} euro in his euro Revolut account")
     public void dannyHasEuroInHisEuroRevolutAccount(double startingBalance) {
-        //System.out.println("Danny has starting account balance of: " + String.valueOf(startingBalance));
         danny.setAccountBalance("EUR", startingBalance);
-        //double newAccountBalance = danny.getAccountBalance("EUR");
-        //System.out.println("Danny's new account balance if: " + String.valueOf(newAccountBalance));
     }
 
     @Given("Danny selects {double} euro as the topUp amount")
     public void danny_selects_euro_as_the_top_up_amount(double topUpAmount) {
-        // Write code here that turns the phrase above into concrete actions
         this.topUpAmount = topUpAmount;
-        //throw new io.cucumber.java.PendingException();
     }
 
-    //@Given("Danny selects his {word} as his topUp method")
     @Given("Danny selects his {paymentService} as his topUp method")
-    //public void danny_selects_his_debit_card_as_his_top_up_method(String topUpSource) {
     public void danny_selects_his_debit_card_as_his_top_up_method(PaymentService topUpSource) {
-        // Write code here that turns the phrase above into concrete actions
         System.out.println("The selected payment service type was " + topUpSource.getType());
         topUpMethod = topUpSource;
     }
 
     @When("Danny tops up")
     public void danny_tops_up() {
-        // Write code here that turns the phrase above into concrete actions
         danny.getAccount("EUR").addFunds(topUpAmount);
-        //throw new io.cucumber.java.PendingException();
     }
 
     @Then("The new balance of his euro account should now be {double}")
     public void the_new_balance_of_his_euro_account_should_now_be(double newBalance) {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
         //Arrange
         double expectedResult = newBalance;
         //Act
